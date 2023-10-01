eb949972a5b5eacea8decdb2ddc9364884789da3
shubham <shubham11mishra@gmail.com>
2023-10-01 08:33:03 -0700

    update

--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ShubhamMishra\Contact\Http\Controllers\Contact\Http\Controllers;
+namespace ShubhamMishra\Contact\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\category;
